e1541df74436b1a0d5cd8e6dec2f0aed9a33c9a2
Patrik Backhuvud <patrik.backhuvud@gmail.com>
2025-03-01 15:59:15 +0100

    Add unit tests for InitAuthenticationRequest
    Introduce `InitAuthenticationRequestTests` class with two test methods, `Create1` and `Create2`, to validate the creation of `InitAuthenticationRequest` objects. The tests ensure correct property assignment and handle optional parameters using FluentAssertions for improved readability.

---
+++ FrejaClientTests/Dto/AuthenticationService/InitAuthenticationRequestTests.cs
@@ -0,0 +1,49 @@
+﻿using FluentAssertions;
+using FrejaClient.Dto.AuthenticationService;
+
+namespace FrejaClientTests.Dto.AuthenticationService
+{
+    [TestClass]
+    public class InitAuthenticationRequestTests
+    {
+        [TestMethod]
+        public void Create1()
+        {
+            var initAuthenticationRequest = new InitAuthenticationRequest(
+                new InitAuthenticationRequestData(
+                    "userInfoType",
+                    "userInfo",
+                    "minRegistrationLevel",
+                    "userConfirmationMethod",
+                    ["attributesToReturn"]
+                    )
+                );
+
+            initAuthenticationRequest.Should().NotBeNull();
+            initAuthenticationRequest.Data.Should().NotBeNull();
+            initAuthenticationRequest.Data.UserInfoType.Should().Be("userInfoType");
+            initAuthenticationRequest.Data.UserInfo.Should().Be("userInfo");
+            initAuthenticationRequest.Data.MinRegistrationLevel.Should().Be("minRegistrationLevel");
+            initAuthenticationRequest.Data.UserConfirmationMethod.Should().Be("userConfirmationMethod");
+            initAuthenticationRequest.Data.AttributesToReturn.Should().BeEquivalentTo(["attributesToReturn"]);
+        }
+
+        [TestMethod]
+        public void Create2()
+        {
+            var initAuthenticationRequest = new InitAuthenticationRequest(
+                new InitAuthenticationRequestData(
+                    "userInfoType",
+                    "userInfo"
+                    )
+                );
+            initAuthenticationRequest.Should().NotBeNull();
+            initAuthenticationRequest.Data.Should().NotBeNull();
+            initAuthenticationRequest.Data.UserInfoType.Should().Be("userInfoType");
+            initAuthenticationRequest.Data.UserInfo.Should().Be("userInfo");
+            initAuthenticationRequest.Data.MinRegistrationLevel.Should().BeNull();
+            initAuthenticationRequest.Data.UserConfirmationMethod.Should().BeNull();
+            initAuthenticationRequest.Data.AttributesToReturn.Should().BeNull();
+        }
+    }
+}
